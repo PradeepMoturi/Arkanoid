6e7b6c103c78569b8ead7f040d0dccb36bf0bce1
Udit Maniyar <uditmaniyar9@gmail.com>
2019-03-01 03:34:42 +0530

    Added install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#Install Dependencies
+sudo apt-get update
+sudo apt-get install qtdeclarative5-dev
+sudo apt-get install qt5-qmake 
+sudo apt-get install qt5-default
+sudo apt-get install qtmultimedia5-dev
+sudo apt-get install libqt5multimediawidgets5
+sudo apt-get install libqt5multimedia5-plugins
+sudo apt-get install libqt5multimedia5
+
