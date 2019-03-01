974e42b4069cef72b6ee3232b8a3a75e90711d69
PradeepMoturi <es16btech11016@iith.ac.in>
2019-03-01 19:59:40 +0530

    Doxygen comments added for start and end menus

--- endmenu.h
+++ endmenu.h
@@ -3,21 +3,50 @@
 
 #include <QMainWindow>
 #include "startpage.h"
-
+/*!
+ * \brief The End Menu class
+ *
+ * This is displayed when you loose.
+ */
 class EndMenu : public QMainWindow
 {
 Q_OBJECT
 
 public:
+    /*!
+     * \brief Constructor for the End Menu.
+     * \param parent
+     *
+     * This adds background, buttons and connections.
+     */
     EndMenu(QWidget *parent = nullptr);
-    void display();
 
 private slots:
+    /*!
+     * \brief Fucntion run when Restart is clicked.
+     *
+     * This sends restart signal to Game.
+     */
     void handleRestart();
+    /*!
+     * \brief Function to run when End Game is clicked.
+     *
+     * This exits the Game safely.
+     */
     void handleEndGame();
 
 private:
+    /*!
+     * \brief Restart Button.
+     *
+     * This is a QPushButton item for restart option.
+     */
     CustomButton* m_restart;
+    /*!
+     * \brief Exit Game Button.
+     *
+     * This is a QPushButton item for Exit game.
+     */
     CustomButton* m_exitGame;
 };
 
